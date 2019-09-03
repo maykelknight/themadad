117eee52618a1316b50be379ac03b35ad4cdaa22
Michał Czapkowicz <czapkowicz@nonstop.co>
2019-09-03 21:14:37 +0200

    slick slider fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -166,6 +166,35 @@ const vueApp = new Vue({
                 });
                 parsedSlides.sort((a, b) => a.number - b.number);
                 this.slides = parsedSlides;
+                $(document).ready(function () {
+                    $('.team-slider').slick({
+                        // asNavFor: '.team-captions',
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        autoplay: true,
+                        autoplaySpeed: 5000,
+                        mobileFirst: true,
+                        dots: true,
+                        arrows: true,
+                        responsive: [
+                            {
+                                breakpoint: 800,
+                                settings: {
+                                    slidesToShow: 2,
+                                    slidesToScroll: 2
+                                }
+                            }
+                        ]
+                    });
+
+                    // $('.team-captions').slick({
+                    //     slidesToShow: 1,
+                    //     slidesToScroll: 1,
+                    //     dots: false,
+                    //     navigation: false
+                    // });
+                });
+
             });
         },
         getTestimonials() {
--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@ AOS.init();
 
 
 $(document).ready(function () {
-    $('.team-slider').slick({
-        // asNavFor: '.team-captions',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        mobileFirst: true,
-        dots: true,
-        arrows: true,
-        responsive: [
-            {
-                breakpoint: 800,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            }
-        ]
-    });
+    // $('.team-slider').slick({
+    //     // asNavFor: '.team-captions',
+    //     slidesToShow: 1,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 5000,
+    //     mobileFirst: true,
+    //     dots: true,
+    //     arrows: true,
+    //     responsive: [
+    //         {
+    //             breakpoint: 800,
+    //             settings: {
+    //                 slidesToShow: 2,
+    //                 slidesToScroll: 2
+    //             }
+    //         }
+    //     ]
+    // });
 
     // $('.team-captions').slick({
     //     slidesToShow: 1,
